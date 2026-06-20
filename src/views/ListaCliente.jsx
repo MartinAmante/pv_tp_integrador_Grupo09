353d0c357686f31b86e9eb84f0f1feeb97bb7ddb
TomasVilte <tomiviltej7@gmail.com>
2026-06-20 02:35:57 -0300

    Realizar peticion GET a la API de clientes. Tomas Vilte

--- src/views/ListaCliente.jsx
+++ src/views/ListaCliente.jsx
@@ -6,6 +6,26 @@ function ListaCliente() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
+    useEffect(() => {
+        obtenerClientes();
+    }, []);
+
+    const obtenerClientes = async () => {
+        try {
+            const respuesta = await fetch(
+                "https://fakestoreapi.com/users"
+            );
+             const datos = await respuesta.json();
+            setClientes(datos);
+
+        } catch (error) {
+             setError("No se pudieron cargar los clientes");
+
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <>
             <h1>Lista de Clientes</h1>
