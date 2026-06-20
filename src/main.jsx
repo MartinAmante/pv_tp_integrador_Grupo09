e67a03835d501dc48e230c3a92c3f1f7793551c8
Llampa Alejandro Matias Salomón <matias.asllampa@gmail.com>
2026-06-20 05:32:54 -0300

    avance context, login, header y logout funcionando - MatiasLlampa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { AdminProvider } from './context/AdminContext.jsx';
 
 createRoot(document.getElementById('root')).render(
+  <AdminProvider>
   <StrictMode>
     <App />
-  </StrictMode>, 
+  </StrictMode>
+  </AdminProvider>
   
 );
