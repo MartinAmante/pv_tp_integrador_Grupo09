4eccea24c801a7d1b0df48ff7ae23adc7de9a424
TomasVilte <tomiviltej7@gmail.com>
2026-06-20 01:40:43 -0300

    Agregando estados de cliente, carga y error. Tomas Vilte

--- src/views/ListaCliente.jsx
+++ src/views/ListaCliente.jsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
+
 function ListaCliente() {
+
+    const [clientes, setClientes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
     return (
         <>
             <h1>Lista de Clientes</h1>
